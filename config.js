83dfe4f33c9c1b4fa27d84fb8e76dda6741f2d94
Tran Khanh <lenovo170494@gmail.com>
2022-06-18 10:08:22 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'LXO';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100014565430298';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng Linh Xe Ôm 19-6';
+const contentCard = 'Chúc Mít luôn mạnh khỏe, học tập tốt, may mắn và tràn đầy niềm vui';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'anh2.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
